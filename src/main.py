ab88f3a4fb99b50f669e9d36a182ab4f673db069
ACd3v <58480609+ACd3v@users.noreply.github.com>
2022-06-09 10:41:45 +0200

    Added get accounts route

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from typing import List
 from urllib import response
 import fastapi as _fastapi
 import sqlalchemy.orm as _orm
@@ -14,10 +15,14 @@ _services.create_database()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/api/account", response_model=List[_schemas.Account])
+def read_accounts(skip: int=0, limit: int=10, db: _orm.Session = _fastapi.Depends(_services.get_db)):
+    accounts = _services.get_accounts(db = db, skip = skip, limit = limit)
+    return accounts
 
 @app.post("/api/account", response_model=_schemas.Account)
 def create_account(account: _schemas.AccountCreate, db: _orm.Session = _fastapi.Depends(_services.get_db)):
-    return _services.create_account(db=db, account=account)
+    return _services.create_account(db = db, account = account)
 
     # @app.get("/items/{item_id}")
     # def read_item(item_id: int, q: Union[str, None] = None):
