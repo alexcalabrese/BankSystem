46610572e2b56c1b4c6458417208ea074325b6f5
ACd3v <58480609+ACd3v@users.noreply.github.com>
2022-06-09 17:29:02 +0200

    Created Admin model

--- pyenv/bank/models.py
+++ pyenv/bank/models.py
@@ -1,1 +1,15 @@
-from django
+
+from unicodedata import name
+from django.db import models
+from .utils import create_random_string_id
+
+
+class Account(models.Model):
+    id = models.CharField(max_length=20, primary_key=True,
+                          default=create_random_string_id, editable=False)
+    name = models.CharField(max_length=50, unique=False)
+    surname = models.CharField(max_length=50, unique=False)
+    balance = models.FloatField(default=0, editable=False)
+
+    def __str__(self):
+        return self.name + self.surname
