f3b41e9a9d85e39f3085825d74a07e0afa85fdcc
ACd3v <58480609+ACd3v@users.noreply.github.com>
2022-06-12 19:50:03 +0200

    Added transactions details to Account

--- pyenv/bank/serializers.py
+++ pyenv/bank/serializers.py
@@ -1,14 +1,54 @@
 from rest_framework import serializers
+
+from bank.utils import validate_float_field
 from .models.account import Account
 from .models.transaction import Transaction
 
 
+class TransactionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Transaction
+        fields = ['id',
+                  'account_from',
+                  'account_to',
+                  'amount']
+
+    def validate(self, data):
+        if data.get('account_from') is None:
+            raise serializers.ValidationError(
+                {"message": "Error 400, sender account is required"})
+        elif data.get('account_to') is None:
+            raise serializers.ValidationError(
+                {"message": "Error 400, receiver account is required"})
+        elif data.get('amount') is None:
+            raise serializers.ValidationError(
+                {"message": "Error 400, amount is required"})
+
+        try:
+            account_from = data.get('account_from')
+            account_to = data.get('account_to')
+        except serializers.NotFound:
+            raise serializers.NotFound(
+                {"message": "Error 404, account not found"})
+
+        amount = validate_float_field(data.get('amount'), "amount")
+
+        if account_from != account_to:
+            if amount < 0:
+                raise serializers.ValidationError(
+                    {'message': 'Error 400, cannot perform negative transfer'})
+            if abs(amount) >= abs(account_from.balance):
+                raise serializers.ValidationError(
+                    {'message': 'Error 400, not enough money',
+                        'current_balance: ': account_from.balance})
+        return data
+
+
 class AccountSerializer(serializers.ModelSerializer):
-    transactions = serializers.PrimaryKeyRelatedField(
+    transactions = TransactionSerializer(
         source='account_from',
-        queryset=Transaction.objects.order_by('created_at'),
-        many=True,
-        required=False)
+        many=True)
 
     class Meta:
         model = Account
@@ -31,41 +71,3 @@ class AccountSerializer(serializers.ModelSerializer):
                 {"message": "Error 400, surname is required"})
         else:
             return surname
-
-
-class TransactionSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Transaction
-        fields = ['id',
-                  'account_from',
-                  'account_to',
-                  'amount']
-
-    def validate(self, data):
-        print("Ciao")
-        if data.get('account_from') is None:
-            raise serializers.ValidationError(
-                {"message": "Error 400, sender account is required"})
-        elif data.get('account_to') is None:
-            raise serializers.ValidationError(
-                {"message": "Error 400, receiver account is required"})
-        elif data.get('amount') is None:
-            raise serializers.ValidationError(
-                {"message": "Error 400, amount is required"})
-
-        try:
-            account_from = data.get('account_from')
-            account_to = data.get('account_to')
-        except serializers.NotFound:
-            raise serializers.NotFound(
-                {"message": "Error 404, account not found"})
-
-        if data.get('amount') < 0:
-            raise serializers.ValidationError(
-                {'message': 'Error 400, cannot perform negative transfer'})
-        elif data.get('amount') > account_from.balance:
-            raise serializers.ValidationError(
-                {'message': 'Error 400, not enough money',
-                    'current_balance: ': account_from.balance})
-        return data
