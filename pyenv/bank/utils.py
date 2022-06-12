9d6f147f28c93de130305e45dc00a68f199d828f
ACd3v <58480609+ACd3v@users.noreply.github.com>
2022-06-12 15:22:24 +0200

    Added some util functions

--- pyenv/bank/utils.py
+++ pyenv/bank/utils.py
@@ -1,5 +1,24 @@
 import secrets
+from rest_framework.exceptions import ValidationError
 
 
 def create_random_string_id():
     return secrets.token_hex(10)
+
+
+def validate_required_field(field, subject):
+    if(field == False):
+        raise ValidationError(
+            {"Message": "Error 400, " + subject + " is required"})
+    else:
+        return field
+
+
+def validate_float_field(field, subject):
+    try:
+        float_field = float(field)
+    except Exception:
+        raise ValidationError(
+            {"Message": "Error 400, " + subject + " must be a number"})
+
+    return float_field
