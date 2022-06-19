07930be93d093b754b5acabb7c1ef8a0218f489a
ACd3v <58480609+ACd3v@users.noreply.github.com>
2022-06-19 12:39:25 +0200

    Changed to on_delete DO_NOTHING

--- pyenv/bank/models/transaction.py
+++ pyenv/bank/models/transaction.py
@@ -12,9 +12,9 @@ class Transaction(models.Model):
     id = models.UUIDField(default=uuid.uuid4,
                           primary_key=True, editable=False, unique=True)
     account_from = models.ForeignKey(
-        Account, related_name='account_from', null=True, on_delete=models.SET_NULL)
+        Account, related_name='account_from', null=True, on_delete=models.DO_NOTHING)
     account_to = models.ForeignKey(
-        Account, related_name='account_to', null=True, on_delete=models.SET_NULL)
+        Account, related_name='account_to', null=True, on_delete=models.DO_NOTHING)
     amount = models.FloatField(default=0)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
