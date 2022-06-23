b1c945424378a6075bbd2445c107fc056d90b7cc
ACd3v <58480609+ACd3v@users.noreply.github.com>
2022-06-23 21:56:37 +0200

    Added selfTransaction to admin page

--- pyenv/bank/admin.py
+++ pyenv/bank/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models.account import Account
 from .models.transaction import Transaction
+from .models.transaction import SelfTransaction
 
 admin.site.register(Account)
 admin.site.register(Transaction)
+admin.site.register(SelfTransaction)
